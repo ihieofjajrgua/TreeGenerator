e18d053a8d8551861a68aa515ab8034650bf7b1f
zhaotong0612 <2248620869@qq.com>
2018-06-05 22:28:18 +0800

    修改树节点在图片上的大小和位置的计算方式

--- src/tree/PictureGenerator.java
+++ src/tree/PictureGenerator.java
@@ -6,13 +6,13 @@ import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 
 public class PictureGenerator {
-	private int left, bWidth = 200, bHeight = 100, padding = 10;
+	private int left, bWidth, bHeight, padding = 10;
 	
 	private int getBWidth(TreeNode root, Graphics g) {
 		if(root == null)
 			return 0;
 		else {
-			int ret = g.getFontMetrics().stringWidth(root.val) + 2 * padding;
+			int ret = g.getFontMetrics().stringWidth(root.val) + 4 * padding;
 			for(TreeNode t: root.childs)
 				ret = Math.max(ret, getBWidth(t, g));
 			return ret;
@@ -20,17 +20,21 @@ public class PictureGenerator {
 	}
 	
 	public BufferedImage generatePicture(TreeNode root) {
-		int[] result = getPicWidthAndHeight(root);
+		int[] result = getTreeWidthAndHeight(root);
 		int tHeight = result[1] - 1, tWidth = result[0];
 		//System.out.println(tWidth + "," + tHeight);
 		
-		BufferedImage bi = new BufferedImage(tWidth * bWidth, tHeight * bHeight, BufferedImage.TYPE_INT_RGB);
+		BufferedImage bi = new BufferedImage(1, 1, BufferedImage.TYPE_INT_RGB);
 		Graphics g = bi.getGraphics();
+		g.setFont(new Font(g.getFont().getFontName(), Font.PLAIN, 20));
+		bWidth = getBWidth(root, g);
+		bHeight = g.getFontMetrics().getHeight() + 4 * padding;
+		
+		bi = new BufferedImage(tWidth * bWidth, tHeight * bHeight, BufferedImage.TYPE_INT_RGB);
+		g = bi.getGraphics();
 		g.setFont(new Font(g.getFont().getFontName(), Font.PLAIN, 20));
 		g.setColor(Color.WHITE);
 		g.fillRect(0, 0, bi.getWidth(), bi.getHeight());
-		bWidth = getBWidth(root, g);
-		//System.out.println(bWidth);
 		
 		g.setColor(Color.BLACK);
 		left = 0;
@@ -45,10 +49,10 @@ public class PictureGenerator {
 		int strHeight = g.getFontMetrics().getHeight();
 		int strDescent = g.getFontMetrics().getDescent();
 		if(root.childs.isEmpty()) {
-			g.drawRect(left + bWidth / 4, height + bHeight / 4, bWidth / 2, bHeight / 2);
 			g.drawString(root.val, left + bWidth / 2 - strWidth / 2, height + bHeight / 2 + strHeight / 2 - strDescent);
+			g.drawRect(left + bWidth / 2 - strWidth / 2 - padding, height + bHeight / 2 - strHeight / 2 - padding, strWidth + 2 * padding, strHeight + 2 * padding);
 			if(height > 0)
-				g.drawLine(left + bWidth / 2, height + bHeight / 4, left + bWidth / 2, height);
+				g.drawLine(left + bWidth / 2, height + padding, left + bWidth / 2, height);
 			left += bWidth;
 			return left - bWidth / 2;
 		}
@@ -57,17 +61,17 @@ public class PictureGenerator {
 			for(int i = 1; i < root.childs.size(); i++)
 				lc = drawNodes(root.childs.get(i), g, height + bHeight);
 			int ret = (fc + lc) / 2;
-			if(height > 0)
-				g.drawLine(ret, height + bHeight / 4, ret, height);
-			g.drawRect(ret - bWidth / 4, height + bHeight / 4, bWidth / 2, bHeight / 2);
 			g.drawString(root.val, ret - strWidth / 2, height + bHeight / 2 + strHeight / 2 - strDescent);
+			if(height > 0)
+				g.drawLine(ret, height + padding, ret, height);
+			g.drawRect(ret - strWidth / 2 - padding, height + bHeight / 2 - strHeight / 2 - padding, strWidth + 2 * padding, strHeight + 2 * padding);
 			g.drawLine(fc, height + bHeight, lc, height + bHeight);
-			g.drawLine(ret, height + bHeight * 3 / 4, ret, height + bHeight);
+			g.drawLine(ret, height + bHeight - padding, ret, height + bHeight);
 			return ret;
 		}
 	}
 	
-	private int[] getPicWidthAndHeight(TreeNode root) {
+	private int[] getTreeWidthAndHeight(TreeNode root) {
 		//System.out.println("root.val = " + root.val);
 		int[] result = new int [2];
 		if(root.childs.isEmpty())
@@ -75,7 +79,7 @@ public class PictureGenerator {
 		else {
 			result[0] = 0; result[1] = 1;
 			for(TreeNode t: root.childs) {
-				int[] res = getPicWidthAndHeight(t);
+				int[] res = getTreeWidthAndHeight(t);
 				result[0] += res[0];
 				result[1] = Math.max(result[1], 1 + res[1]);
 				//System.out.println(result[0]);
