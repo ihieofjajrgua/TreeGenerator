0f640614df3c207c43a89ad6877326823fc26f1d
zhaotong0612 <2248620869@qq.com>
2018-06-22 14:57:25 +0800

    将绘制子树的过程与绘制节点的过程分离

--- src/tree/PictureGenerator.java
+++ src/tree/PictureGenerator.java
@@ -2,19 +2,41 @@ package tree;
 
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.FontMetrics;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 
 public class PictureGenerator {
 	private int left, bWidth, bHeight, padding = 10;
 	
-	private int getBWidth(TreeNode root, Graphics g) {
+	private int getBWidth(TreeNode root, FontMetrics f) {
 		if(root == null)
 			return 0;
 		else {
-			int ret = g.getFontMetrics().stringWidth(root.val) + 4 * padding;
+			int ret = Math.min(f.stringWidth("啊啊啊啊啊啊啊啊啊啊"), 
+				f.stringWidth(root.val)) + 4 * padding;
 			for(TreeNode t: root.childs)
-				ret = Math.max(ret, getBWidth(t, g));
+				ret = Math.max(ret, getBWidth(t, f));
+			return ret;
+		}
+	}
+	
+	private int getBHeight(TreeNode root, FontMetrics f) {
+		if(root == null)
+			return 0;
+		else {
+			int lineStart = 0, height = 0;
+			for(int i = 0; i < root.val.length(); i++) {
+				if(f.stringWidth(root.val.substring(lineStart, i)) > bWidth) {
+					lineStart = i;
+					height++;
+				}
+			}
+			height++;
+			int ret = height * f.getHeight() + 4 * padding;
+			
+			for(TreeNode t: root.childs)
+				ret = Math.max(ret, getBHeight(t, f));
 			return ret;
 		}
 	}
@@ -27,8 +49,9 @@ public class PictureGenerator {
 		BufferedImage bi = new BufferedImage(1, 1, BufferedImage.TYPE_INT_RGB);
 		Graphics g = bi.getGraphics();
 		g.setFont(new Font(g.getFont().getFontName(), Font.PLAIN, 20));
-		bWidth = getBWidth(root, g);
-		bHeight = g.getFontMetrics().getHeight() + 4 * padding;
+		bWidth = getBWidth(root, g.getFontMetrics());
+		bHeight = getBHeight(root, g.getFontMetrics());
+		System.out.println(bWidth + "," + bHeight);
 		
 		bi = new BufferedImage(tWidth * bWidth, tHeight * bHeight, BufferedImage.TYPE_INT_RGB);
 		g = bi.getGraphics();
@@ -38,35 +61,40 @@ public class PictureGenerator {
 		
 		g.setColor(Color.BLACK);
 		left = 0;
-		drawNodes(root, g, -bHeight);
+		drawSubTree(root, g, -bHeight);
 		g.dispose();
 		
 		return bi;
 	}
 	
-	private int drawNodes(TreeNode root, Graphics g, int height) {
-		int strWidth = g.getFontMetrics().stringWidth(root.val);
+	private int drawNode(TreeNode node, Graphics g, int mid, int height) {
+		int strWidth = g.getFontMetrics().stringWidth(node.val);
 		int strHeight = g.getFontMetrics().getHeight();
 		int strDescent = g.getFontMetrics().getDescent();
+		
+		g.drawString(node.val, mid - strWidth / 2, height + bHeight / 2 + strHeight / 2 - strDescent);
+		g.drawRect(mid - strWidth / 2 - padding, height + bHeight / 2 - strHeight / 2 - padding, strWidth + 2 * padding, strHeight + 2 * padding);
+		if(height > 0)
+			g.drawLine(mid, height + bHeight / 2 - strHeight / 2 - padding, mid, height);
+		
+		return height + bHeight / 2 + strHeight / 2 + padding;
+	}
+	
+	private int drawSubTree(TreeNode root, Graphics g, int height) {
 		if(root.childs.isEmpty()) {
-			g.drawString(root.val, left + bWidth / 2 - strWidth / 2, height + bHeight / 2 + strHeight / 2 - strDescent);
-			g.drawRect(left + bWidth / 2 - strWidth / 2 - padding, height + bHeight / 2 - strHeight / 2 - padding, strWidth + 2 * padding, strHeight + 2 * padding);
-			if(height > 0)
-				g.drawLine(left + bWidth / 2, height + padding, left + bWidth / 2, height);
+			drawNode(root, g, left + bWidth / 2, height);
 			left += bWidth;
 			return left - bWidth / 2;
 		}
 		else {
-			int fc = drawNodes(root.childs.get(0), g, height + bHeight), lc = fc;
+			int fc = drawSubTree(root.childs.get(0), g, height + bHeight), lc = fc;
 			for(int i = 1; i < root.childs.size(); i++)
-				lc = drawNodes(root.childs.get(i), g, height + bHeight);
+				lc = drawSubTree(root.childs.get(i), g, height + bHeight);
 			int ret = (fc + lc) / 2;
-			g.drawString(root.val, ret - strWidth / 2, height + bHeight / 2 + strHeight / 2 - strDescent);
-			if(height > 0)
-				g.drawLine(ret, height + padding, ret, height);
-			g.drawRect(ret - strWidth / 2 - padding, height + bHeight / 2 - strHeight / 2 - padding, strWidth + 2 * padding, strHeight + 2 * padding);
+			int bottom = drawNode(root, g, ret, height);
+			
 			g.drawLine(fc, height + bHeight, lc, height + bHeight);
-			g.drawLine(ret, height + bHeight - padding, ret, height + bHeight);
+			g.drawLine(ret, bottom, ret, height + bHeight);
 			return ret;
 		}
 	}
